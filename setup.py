59359430fc99fb64a08fe80689bb505af8e72775
Andres Chavez Ulfe <achavez@Andres-Chavez.local>
2016-11-14 17:03:18 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os.path
+from setuptools import setup, find_packages
+
+package_dir = os.path.abspath(os.path.dirname(__file__))
+version_file = os.path.join(package_dir, "version")
+with open(version_file) as version_file_handle:
+    version = version_file_handle.read()
+
+setup(
+    name = "middleware-security",
+    version = version,
+    description = "Falcon Security middlware",
+    package_dir = {"":"src"},
+    packages = find_packages("src"),
+    install_requires=["falcon"],
+    author = 'DevTeam RCP',
+    author_email = 'devteam@rcp.pe',
+    url = 'https://git.rcp.pe/devteam/middleware-security',
+    keywords = ['falcon', 'rest', 'http']
+)
---
+++ src/middleware_security/middleware.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import requests
+from .api.exception import HTTPException
+from requests.exceptions import HTTPError
+
+class Security(object):
+
+    def __init__(self, config):
+        self.config = config
+
+    def process_request(self, req, resp):
+        try:
+            if req.method == 'OPTIONS':
+                return
+
+            with requests.Session() as s:
+                s.trust_env = False
+                r = s.get('%s/%s' % (self.config["api"], 'verify_token'),
+                    headers={'Authorization': req.auth}
+                )
+                r.raise_for_status()
+
+                data = r.json()
+                req.context["token_user"] = data["token"]["user"]
+                req.context["token_roles"] = data["token"]["roles"]
+        except HTTPError as e:
+            status_code = r.status_code or 500
+            raise HTTPException(status_code)
+        except Exception as e:
+            raise HTTPException(401)

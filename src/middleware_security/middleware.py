865bed7da89c80521e0ef3d2f42d548d93d18a7c
Andrés Chávez <achavezu89@gmail.com>
2017-08-25 11:25:31 -0500

    Change middleware

--- src/middleware_security/middleware.py
+++ src/middleware_security/middleware.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-import requests
+import jwt
 from falcon_exceptions import HTTPException
-from requests.exceptions import HTTPError
 
 class AuthVerify(object):
 
@@ -9,22 +8,22 @@ class AuthVerify(object):
         self.config = config
 
     def process_request(self, req, resp):
-        try:
-            if req.method == 'OPTIONS':
-                return
+        if req.method == 'OPTIONS':
+            return
 
-            with requests.Session() as s:
-                s.trust_env = False
-                r = s.get('%s/%s' % (self.config["api"], 'verify_token'),
-                    headers={'Authorization': req.auth}
-                )
-                r.raise_for_status()
+        secure_paths = self.config['security']['secure_paths']
+        current_path = req.path
 
-                data = r.json()
-                req.context["token_user"] = data["token"]["user"]
-                req.context["token_roles"] = data["token"]["roles"]
-        except HTTPError as e:
-            status_code = r.status_code or 500
-            raise HTTPException(status_code)
-        except Exception as e:
-            raise HTTPException(401)
+        if current_path in secure_paths:
+            return
+
+        if not req.auth:
+            raise HTTPException(401, "Authorization token not send")
+
+        bearer_token = req.auth
+        bearer_token = bearer_token.split(' ')[1]
+
+        decoded_token = jwt.decode(bearer_token, verify=False)
+
+        req.context['token_user'] = decoded_token['user']
+        req.context['token_scopes'] = decoded_token['scopes']

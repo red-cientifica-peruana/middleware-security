a74f0379e99a308a279e08b8adde373768e4eb61
Andrés Chávez <achavezu89@gmail.com>
2017-08-29 12:45:44 -0500

    Update utils.py

--- src/middleware_security/utils.py
+++ src/middleware_security/utils.py
@@ -7,17 +7,21 @@ def scope_verify(scope):
     def method_decorator(func):
         def method_wrapper(*args, **kwargs):
             context = args[1].context
-            token_scopes = context['token_scopes']
-            parts_scope = scope.split(':')
+            
+            if not 'token_scopes' in context:
+                func(*args, **kwargs)
+            else:
+                token_scopes = context['token_scopes']
+                parts_scope = scope.split(':')
 
-            if len(parts_scope) < 3 or len(parts_scope) > 3:
-                raise HTTPException(500, "The scope was not set correctly")
+                if len(parts_scope) < 3 or len(parts_scope) > 3:
+                    raise HTTPException(500, "The scope was not set correctly")
 
-            if (parts_scope[0] not in token_scopes or
-                parts_scope[1] not in token_scopes[parts_scope[0]] or
-                parts_scope[2] not in token_scopes[parts_scope[0]][parts_scope[1]]):
-                raise HTTPException(403, "You are not authorized to perform this action")
+                if (parts_scope[0] not in token_scopes or
+                    parts_scope[1] not in token_scopes[parts_scope[0]] or
+                    parts_scope[2] not in token_scopes[parts_scope[0]][parts_scope[1]]):
+                    raise HTTPException(403, "You are not authorized to perform this action")
 
-            func(*args, **kwargs)
+                func(*args, **kwargs)
         return method_wrapper
     return method_decorator

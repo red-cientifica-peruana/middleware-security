40f05a0534dec27eeb2325700a8676f9afb32975
Andrés Chávez <achavezu89@gmail.com>
2017-09-06 12:16:22 -0500

    Update utils.py

--- src/middleware_security/utils.py
+++ src/middleware_security/utils.py
@@ -9,7 +9,7 @@ def scope_verify(scope=None):
             scope_obj = args[0].scope if not scope else scope
             context = args[1].context
             
-            if isinstace(scope, dict):
+            if isinstance(scope, dict):
                 scope_obj = scope_obj.get(func.__name__, None)
             
             if not 'token_scopes' in context:
